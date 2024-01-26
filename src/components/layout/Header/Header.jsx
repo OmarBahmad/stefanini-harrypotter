9b4cef4aaf10c34f16d2fdff2a4f58fd981e02cc
Omar Bahmad <omar.bahmad@bideafactory.com>
2024-01-26 09:54:44 -0300

    styles: minor bugs

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -23,7 +23,7 @@ export const Header = ({ onHouseSelected, initialValue, onSearch }) => {
             <MobileMenu items={menuItems} onHouseSelected={onHouseSelected} />
             <div className="header__logo">
               <img
-                src="src/assets/images/logo-hp.png"
+                src="/assets/images/logo-hp.png"
                 alt="harry-potter-logo"
               />
             </div>
@@ -33,7 +33,7 @@ export const Header = ({ onHouseSelected, initialValue, onSearch }) => {
           <>
             <a className="header__logo" href="/">
               <img
-                src="src/assets/images/logo-hp.png"
+                src="/assets/images/logo-hp.png"
                 alt="harry-potter-logo"
               />
             </a>
