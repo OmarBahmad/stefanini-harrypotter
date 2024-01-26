0100471a15e4c48c2edbf7ad2d3af8416afe7532
Omar Bahmad <omar.bahmad@bideafactory.com>
2024-01-26 04:29:12 -0300

    feat: finished cards home

---
+++ src/components/layout/Home/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import "../../../../sass/components/loader.scss";
+
+export const Loader = () => {
+  return (
+    <div className="loader">
+      {/* Inclua animações ou ícones relevantes aqui */}
+      <div className="loading-broom">🧹</div>
+    </div>
+  );
+};
